682b6024284abe80de5dd358f0120d2cbac0014b
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2025-04-18 15:24:57 -0400

    update

---
+++ JavaScript-Fundamentals/6-comparison/script.js
@@ -0,0 +1,9 @@
+let a = 5 > 4 // true
+let b = "apple" > "pineapple" // false
+let c = "2" > "12" // true
+let d = undefined == null // true
+let e = undefined === null // false
+let f = null == "\n0\n" // true
+let g = null === +"\n0\n" // false
+
+console.log(e);
