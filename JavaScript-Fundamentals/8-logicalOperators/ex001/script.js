7dc071a54d7a74d20f6b7d672e21473a334122ce
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2025-04-18 15:57:33 -0400

    update

---
+++ JavaScript-Fundamentals/8-logicalOperators/ex001/script.js
@@ -0,0 +1,10 @@
+if (age >= 14 && age <= 90);
+
+if (!(age >= 14 && age <= 90)) {
+    // age is NOT between 14 and 90 inclusively
+}
+
+if (age < 14 || age > 90) {
+    // age is NOT between 14 and 90 inclusively
+}
+
